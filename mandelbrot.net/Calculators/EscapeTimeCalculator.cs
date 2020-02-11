f740e4847601d0e8d3bd0d6c3688ab160aef0cbb
gilmae <avocadia@fastmail.fm>
2020-02-11 14:44:17 +1100

    Add Periodocity checking

--- mandelbrot.net/Calculators/EscapeTimeCalculator.cs
+++ mandelbrot.net/Calculators/EscapeTimeCalculator.cs
@@ -14,10 +14,15 @@ namespace mandelbrot.net
 
         public ICalculator.Result Calculate(double real, double imag)
         {
+            // Check that the point isn't in the main cardioid or the period-2 bulb
             if (Math.Pow(real + 1.0, 2.0) + Math.Pow(imag, 2.0) <= MainCardioidBounds)
             {
                 return new ICalculator.Result { Escaped = false, Iterations = int.MaxValue };
             }
+
+            double rOld = double.MaxValue;
+            double iOld = double.MaxValue;
+            double periodocity = 20.0;
             
             int iteration = 1;
             double rsquare = 0.0;
@@ -30,10 +35,23 @@ namespace mandelbrot.net
             {
                 x = rsquare - isquare + real;
                 y = zsquare - rsquare - isquare + imag;
+
+                // If the function has encountered these coordinates before, then we will encounter them again.
+                // Which means we will be in a loop, i.e. there is no escape, it's in the mandelbrot set.
+                if (x == rOld && y == iOld)
+                {
+                    return new ICalculator.Result { Escaped = false, Iterations = _maxIterations };
+                }
                 rsquare = x * x;
                 isquare = y * y;
                 zsquare = (x + y) * (x + y);
                 iteration += 1;
+
+                if (iteration%periodocity == 0)
+                {
+                    rOld = rsquare;
+                    iOld = isquare;
+                }
             }
 
             return new ICalculator.Result { Escaped = iteration < _maxIterations, Iterations = iteration };
